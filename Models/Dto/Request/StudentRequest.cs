add9695bbb4dcfe210aeb7616984d311482046fd
Chippo156 <vovannghiahiep150623@gmail.com>
2025-09-26 12:49:44 +0700

    [Models] [Dto] Add

---
+++ Models/Dto/Request/StudentRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models.Dto.Request
+{
+    public class StudentRequest
+    {
+        [Required(ErrorMessage = "MSSV is required")]
+        public string MSSV { get; set; } = string.Empty;
+        [Required(ErrorMessage = "UserId is required")]
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "ClassId is required")]
+        public int ClassId { get; set; }
+    }
+}
--- Models/Dto/Request/UserRequest.cs
+++ Models/Dto/Request/UserRequest.cs
@@ -1,12 +1,19 @@
-﻿namespace StudentManagement.Models.Dto.Request
+﻿using StudentManagement.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models.Dto.Request
 {
     public class UserRequest
     {
+        [Required]
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
+        public Gender Gender { get; set; } = Gender.MALE;
+        public string Address { get; set; } = string.Empty;
+        public string AvatarUrl { get; set; } = string.Empty;
         public int RoleId { get; set; }
     }
 }
