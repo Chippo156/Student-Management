019204167c295bec8ec4efc92b9f09e180855aa5
Đat <ngodat200319@gmail.com>
2025-10-27 23:28:10 +0700

    feat(curriculumCourseService): update getCoursesByStudentDepartment to accept semesterId and filterType parameters

--- src/service/curriculumCourseService.js
+++ src/service/curriculumCourseService.js
@@ -1,10 +1,16 @@
 import axios from '../until/customize-axios';
 
 const curriculumCourseService = {
-  getCoursesByStudentDepartment: async () => {
+  getCoursesByStudentDepartment: async (semesterId, filterType) => {
     try {
       const response = await axios.get(
-        '/api/CurriculumCourse/GetCoursesByStudentDepartment'
+        '/api/CurriculumCourse/GetCoursesByStudentDepartment',
+        {
+          params: {
+            semesterId,
+            filterType,
+          },
+        }
       );
       return response.data;
     } catch (error) {
